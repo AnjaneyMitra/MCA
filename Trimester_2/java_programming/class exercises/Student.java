d41862f7c04542f3b72161ea773aeb3b8f09d952
AnjaneyMitra <mitraanjaney@gmail.com>
2024-10-22 10:09:21 +0530

    upload Student.java file for class exercise

--- Trimester_2/java_programming/class exercises/Student.java
+++ Trimester_2/java_programming/class exercises/Student.java
@@ -16,4 +16,19 @@ public class Student{
         System.out.println("Grade: " + grade);
         System.out.println();   
     }
+
+    public static void main(String[] args){
+        Student student1 = new Student("John", 12);
+        student1.displayDetails();
+        student1.displayGrade();
+
+        Student student2 = new Student("Jane", 11);
+        student2.displayDetails();
+        student2.displayGrade();
+
+    }
+    Student student3 = new Student("Mike", 10);
+    System.out.println("Name: " + student3.name);
+    System.out.println("Grade: " + student3.grade);
+    student3.displayDetails();
 }
