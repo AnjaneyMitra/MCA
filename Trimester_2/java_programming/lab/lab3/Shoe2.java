15d5677eb5cffdef3911c2c4073675a19b6ab837
AnjaneyMitra <mitraanjaney@gmail.com>
2024-11-08 09:54:53 +0530

    added string buffer

--- Trimester_2/java_programming/lab/lab3/Shoe2.java
+++ Trimester_2/java_programming/lab/lab3/Shoe2.java
@@ -34,12 +34,56 @@ class Sneaker extends Shoe {
     }
 }
 
+class MyString extends Shoe {
+    private String value;
+    
+    public MyString(String brand, String value) {
+        super(brand);
+        this.value = value;
+    }
+    
+    @Override
+    public void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Value: " + value);
+    }
+}
+
+class StringBuffer extends Shoe {
+    private StringBuilder buffer;
+    
+    public StringBuffer(String brand) {
+        super(brand);
+        this.buffer = new StringBuilder();
+    }
+    
+    public StringBuffer append(String str) {
+        buffer.append(str);
+        return this;
+    }
+    
+    public StringBuffer reverse() {
+        buffer.reverse();
+        return this;
+    }
+    
+    @Override
+    public void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Buffer: " + buffer.toString());
+    }
+}
 
 public class Shoe2 {
     public static void main(String[] args) {
-
         Sneaker sneaker = new Sneaker("Nike", "Red");
 
         sneaker.setBrand("Adidas").display();
+        
+        MyString myString = new MyString("Anjaney", "Mitra");
+        myString.display();
+        
+        StringBuffer stringBuffer = new StringBuffer("StringBuffer");
+        stringBuffer.append("Hello").append("World").reverse().display();
     }
 }
